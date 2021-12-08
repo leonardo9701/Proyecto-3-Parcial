dfa4aaaa76c8f68a72355c6729cc009eb6be3180
leonardo9701 <a18310011@ceti.mx>
2021-12-07 23:19:29 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Proyecto_3_Parcial
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.circularButtom1 = new Proyecto_3_Parcial.CircularButtom();
+            this.circularButtom2 = new Proyecto_3_Parcial.CircularButtom();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Proyecto_3_Parcial.Properties.Resources.L9;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(689, 485);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(86)))), ((int)(((byte)(192)))));
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(360, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Comencemos";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Form1_button);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 334);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(384, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(86)))), ((int)(((byte)(192)))));
+            this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(414, 413);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(243, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Siguiente";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Form1_button2);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(86)))), ((int)(((byte)(192)))));
+            this.button3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(356, 291);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(248, 45);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Regresemos";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Form1_button);
+            // 
+            // circularButtom1
+            // 
+            this.circularButtom1.BackColor = System.Drawing.Color.Red;
+            this.circularButtom1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.circularButtom1.Location = new System.Drawing.Point(133, 249);
+            this.circularButtom1.Name = "circularButtom1";
+            this.circularButtom1.Size = new System.Drawing.Size(151, 145);
+            this.circularButtom1.TabIndex = 7;
+            this.circularButtom1.Text = "No";
+            this.circularButtom1.UseVisualStyleBackColor = false;
+            this.circularButtom1.Click += new System.EventHandler(this.Form1_button_No);
+            // 
+            // circularButtom2
+            // 
+            this.circularButtom2.BackColor = System.Drawing.Color.Green;
+            this.circularButtom2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.circularButtom2.Location = new System.Drawing.Point(397, 249);
+            this.circularButtom2.Name = "circularButtom2";
+            this.circularButtom2.Size = new System.Drawing.Size(151, 145);
+            this.circularButtom2.TabIndex = 8;
+            this.circularButtom2.Text = "Si";
+            this.circularButtom2.UseVisualStyleBackColor = false;
+            this.circularButtom2.Click += new System.EventHandler(this.Form1_button_si);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(86)))), ((int)(((byte)(192)))));
+            this.button4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(226, 380);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(248, 74);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Volvamos al inicio";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 485);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.circularButtom2);
+            this.Controls.Add(this.circularButtom1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_keyCode);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button3;
+        private CircularButtom circularButtom1;
+        private CircularButtom circularButtom2;
+        private Button button4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+namespace Proyecto_3_Parcial
+{
+    public partial class Form1 : Form
+    {
+        public int actual = 0;
+        public string nombre = "";
+        public string gracioso = "";
+        public string interesante = "";
+        public string narrativa = "";
+        public string descripcion = "";
+        public string argumentos = "";
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L1;
+            actual++;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            textBox1.Visible = false;
+            circularButtom2.Visible = false;
+            circularButtom1.Visible = false;
+        }
+
+        private void Form1_keyCode(object sender, KeyPressEventArgs e)
+        {
+            if (actual == 1)
+            {
+                pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L2;
+                actual++;
+                button1.Visible = true;
+                button1.FlatAppearance.BorderSize = 0;
+                button1.FlatStyle = FlatStyle.Flat;
+            }
+        }
+
+        private void Form1_button(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L3;
+            button2.Visible = true;
+            button3.Visible = false;
+            textBox1.Visible = true;
+            actual++;
+        }
+
+        private void Form1_button2(object sender, EventArgs e)
+        {
+            nombre = textBox1.Text;
+            if (nombre.Length > 0)
+            {
+                button2.Visible = false;
+                textBox1.Visible = false;
+                pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L4;
+                circularButtom2.Visible = true;
+                circularButtom1.Visible = true;
+                actual++;
+            }
+            else
+            {
+                button2.Visible = false;
+                textBox1.Visible = false;
+                pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L12;
+                button3.Visible = true;
+            }
+        }
+
+        private void Form1_button_si(object sender, EventArgs e)
+        {
+            if (actual == 4)
+            {
+                gracioso = "y";
+                actual++;
+                pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L5;
+            } else if (actual == 5)
+            {
+                interesante = "y";
+                actual++;
+                pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L6;
+            } else if (actual == 6)
+            {
+                narrativa = "y";
+                actual++;
+                pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L7;
+            } else if (actual == 7)
+            {
+                descripcion = "y";
+                actual++;
+                pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L8;
+            } else if (actual == 8)
+            {
+                argumentos = "y";
+                if (gracioso == "y")
+                {
+                    if (interesante == "y")
+                    {
+                        circularButtom2.Visible = false;
+                        circularButtom1.Visible = false;
+                        pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L9;
+                        button4.Visible = true;
+                    }
+                    else
+                    {
+                        if (narrativa == "y")
+                        {
+                            if (descripcion == "y")
+                            {
+                                if (argumentos == "y")
+                                {
+                                    circularButtom2.Visible = false;
+                                    circularButtom1.Visible = false;
+                                    pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L10;
+                                    button4.Visible = true;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            circularButtom2.Visible = false;
+                            circularButtom1.Visible = false;
+                            pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L11;
+                            button4.Visible = true;
+                        }
+                    }
+                }
+                else
+                {
+                    if (interesante == "y")
+                    {
+                        if (narrativa == "y")
+                        {
+                            if (descripcion == "y")
+                            {
+                                if (argumentos == "y")
+                                {
+                                    circularButtom2.Visible = false;
+                                    circularButtom1.Visible = false;
+                                    pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L10;
+                                    button4.Visible = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void Form1_button_No(object sender, EventArgs e)
+        {
+            if (actual == 4)
+            {
+                actual++;
+                pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L5;
+            } else if (actual == 5)
+            {
+                actual++;
+                pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L6;
+            } else if (actual == 6)
+            {
+                actual++;
+                pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L7;
+            } else if (actual == 7)
+            {
+                actual++;
+                pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L8;
+            } else if (actual == 8)
+            {
+                if (gracioso == "y")
+                {
+                    if (interesante == "y")
+                    {
+                        circularButtom2.Visible = false;
+                        circularButtom1.Visible = false;
+                        pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L9;
+                        button4.Visible = true;
+                    }
+                    else
+                    {
+                        if (narrativa == "y")
+                        {
+                            if (descripcion == "y")
+                            {
+                                if (argumentos == "y")
+                                {
+                                    circularButtom2.Visible = false;
+                                    circularButtom1.Visible = false;
+                                    pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L10;
+                                    button4.Visible = true;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            circularButtom2.Visible = false;
+                            circularButtom1.Visible = false;
+                            pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L11;
+                            button4.Visible = true;
+                        }
+                    }
+                }
+                else
+                {
+                    if (interesante == "y")
+                    {
+                        if (narrativa == "y")
+                        {
+                            if (descripcion == "y")
+                            {
+                                if (argumentos == "y")
+                                {
+                                    circularButtom2.Visible = false;
+                                    circularButtom1.Visible = false;
+                                    pictureBox1.Image = Proyecto_3_Parcial.Properties.Resources.L10;
+                                    button4.Visible = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
